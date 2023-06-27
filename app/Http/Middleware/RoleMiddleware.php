933ccc621719ac8cc29e704d9378b95ab946fcfe
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2023-06-27 12:02:19 -0300

    feat: Create travels and tests

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $role): Response
+    {
+        if (!auth()->check()) {
+            abort(401);
+        }
+
+        /** @var User $user */
+        $user = auth()->user();
+
+        if (! $user->roles()->where('name', $role)->exists()) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
